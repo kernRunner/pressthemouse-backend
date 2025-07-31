734b692bc8270e4c19dc1c55e91824ffcd9f015f
Marco Huber <marco.huber00@gmail.com>
2025-07-31 13:19:56 +0200

    update

--- index.js
+++ index.js
@@ -74,4 +74,5 @@ app.get('/leaderboard', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://0.0.0.0:${PORT}`));
+
